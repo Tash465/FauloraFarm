b68d48ef1980729ae4de7e5ad4fba767c284ad06
Talia Nyaga <talianyaga6@gmail.com>
2026-04-01 01:30:30 +0300

    Fix auto superuser creation after migrations

--- products/apps.py
+++ products/apps.py
@@ -1,16 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
 
     def ready(self):
-        # This code runs once when the app starts
         from django.contrib.auth.models import User
-        username = 'T490s9'   
-        password = '27962@Ikingi'  
-        email = 'talianyaga6@gmail.com'
 
-        # Check if the user already exists
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
+        def create_admin(sender, **kwargs):
+            username = 'T490s9'
+            password = '27962@Ikingi'
+            email = 'talianyaga6@gmail.com'
+
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
+
+        post_migrate.connect(create_admin, sender=self)
